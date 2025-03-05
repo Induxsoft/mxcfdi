71988a34e9d31e348fe44aa9c6b149d1496dedad
Candelario <candelario.hernandez@induxsoft.net>
2025-03-04 18:25:37 -0600

    Aplicación factura global

---
+++ src/mxcfdi/_static/js/fact-global.js
@@ -0,0 +1,162 @@
+
+document.addEventListener("DOMContentLoaded",()=>{fglobal.init();});
+
+var fglobal=
+{
+    text_notas:"",
+    init()
+    {
+        this.check_ticket=document.getElementById("check_ticket");
+        this.check_remision=document.getElementById("check_remision");
+        this.btn_change=document.getElementById("btn_change");
+        this.form_factura_global=document.getElementById("form_factura_global");
+        this.fecha_range=document.getElementById("fecha_range");
+        this.cconsumo=document.getElementById("cconsumo");
+        this.notas=document.getElementById("notas");
+        this.btn_save_orden=document.getElementById("btn_save_orden");
+        this.check_all=document.getElementById("check_all");
+    
+        this.factura_global_detalle=document.getElementById("factura_global_detalle");
+
+        if(this.check_ticket)this.check_ticket.addEventListener("change",()=>
+        {
+            this.ClickBtnFechaRange();
+            this.form_factura_global.submit();
+        });
+
+        if(this.check_remision)this.check_remision.addEventListener("change",()=>
+        {
+            this.ClickBtnFechaRange();
+            this.form_factura_global.submit();
+        });
+
+        if(this.btn_change)this.btn_change.addEventListener("click",()=>{this.ChangeFilter();});
+        if(this.fecha_range)this.fecha_range.onChanging=(oldvalue,newvalue)=>
+        {
+            if(!newvalue || !this.notas)return;
+            this.notas.value=this.text_notas+" "+newvalue.start+ " al " +newvalue.end;
+        }
+        
+        if(this.btn_save_orden)this.btn_save_orden.addEventListener("click",()=>{this.SaveDetalle();});
+
+        if(this.check_all)this.check_all.addEventListener("change",()=>{this.CheckedAll();});
+
+        setTimeout(() => 
+        {
+            if(this.fecha_range._btnEdit)this.fecha_range._btnEdit.style.cssText="display:none";    
+            
+            this.fechaini=document.querySelector("input[name='fechaini']");
+            this.fechafin=document.getElementById("input[name='fechafin']");
+
+            this.factura_global_detalle.Columns[0]={type:this.factura_global_detalle.EdiTable.Const.Columns.Types.Check, field:"field_check",default:"No"};
+
+        }, 100);
+        
+    },
+    Enable(enable=true,elements=null)
+    {
+        if(!this.form_factura_global)return;
+
+        if(!elements) elements=this.form_factura_global.querySelector("input,select,textarea");
+
+        for (let i = 0; i < elements.length; i++) 
+        {
+            const element = elements[i];
+            if(element)
+            {
+                if(enable)
+                {
+                    element.removeAttribute("disabled");
+                    element.removeAttribute("readonly");
+                }
+                else
+                {
+                    // element.setAttribute("disabled",true);
+                    element.setAttribute("readonly",true);
+                }
+            }   
+        }
+    },
+    ChangeFilter()
+    {
+        this.Enable(true,[this.cconsumo,this.fecha_range]);
+        this.ClickBtnFechaRange("edit");
+
+        if(this.btn_change)
+        {
+            if(this.btn_change.textContent.toLocaleLowerCase()=="aplicar")
+            {
+                if(!this.check_ticket.checked && !this.check_remision.checked)
+                {
+                    alert("Es necesario indique al menos un tipo de documento Ticket y/o Remisión.");
+                    return;
+                }
+                this.ClickBtnFechaRange();
+                this.btn_change.type="submit";
+            }
+            this.btn_change.textContent="Aplicar";
+            
+        }
+    },
+    ClickBtnFechaRange(btn="")
+    {
+        switch (btn) 
+        {
+            case "edit":
+                if(fglobal.fecha_range._btnEdit)fglobal.fecha_range._btnEdit.click();
+                break;
+        
+            default:
+                if(fglobal.fecha_range._btnDone)fglobal.fecha_range._btnDone.click();
+                break;
+        }
+        
+    },
+    SaveDetalle()
+    {
+        if(this.fechaini && this.fechaini.value.trim()=="")
+        {
+            alert("Debe seleccionar una fecha de inicio");
+            return;
+        }
+        if(this.fechafin && this.fechafin.value.trim()=="")
+        {
+            alert("Debe seleccionar una fecha fin");
+            return;
+        }
+        
+        var Array=[];
+
+        for (let i = 0; i < this.factura_global_detalle.DataArray.length; i++) 
+        {
+            const element = this.factura_global_detalle.DataArray[i];
+            if(element && tools.ParseBool(element.field_check??false))
+            {
+                var item={venta:element.sys_pk}
+                Array.push(item);
+            }
+        }
+
+        if(Array.length<1)
+        {
+            alert("Debe seleccionar algún documento Ticket o Remisión");
+            return;
+        }
+
+        var details=
+        {
+            detalle:Array
+        }
+        InduxsoftCrudlModel.Submit(this.form_factura_global,details);
+    },
+    CheckedAll()
+    {
+        let checked=this.check_all.checked;
+        for (let i = 0; i < this.factura_global_detalle.DataArray.length; i++) 
+        {
+            const element = this.factura_global_detalle.DataArray[i];
+            element["field_check"]=checked?"Sí":"No";
+        }
+        this.factura_global_detalle._printRows();
+    }
+}
