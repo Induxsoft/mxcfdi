4ea2f1a21b4e5cc96dccdb85265ec95b34fc25a0
Candelario <candelario.hernandez@induxsoft.net>
2025-08-04 17:56:19 -0600

    Insertar gasto desde un CFDI

--- src/mxcfdi/_static/js/load-cfdi.js
+++ src/mxcfdi/_static/js/load-cfdi.js
@@ -3,11 +3,122 @@ document.addEventListener("DOMContentLoaded",()=>{load_cfdi.init()});
 
 var load_cfdi=
 {
+    module:"",
     init()
     {
         this.proveedor=document.getElementById("proveedor");
         this.tbl_datail=document.getElementById("tbl_datail");
         this.form_detalle=document.getElementById("form-detalle");
+
+        if(this.proveedor)this.proveedor.addEventListener("change",()=>
+        {
+            let data=this.proveedor.getValue();
+            if(this.module=="gasto" && data && Object.keys(data).length >0)
+            {
+                for (let i = 0; i < this.tbl_datail.DataArray.length; i++)
+                {
+                    var item = this.tbl_datail.DataArray[i];
+                    item["beneficiario"]=data.nombre;
+                    this.tbl_datail.UpdateRow(i);
+                }
+            }
+        });
+    },
+    ValidateModule()
+    {
+        switch (load_cfdi.module) 
+        {
+            case "gasto":
+                for (let i = 0; i < this.tbl_datail.DataArray.length; i++)
+                {
+                    var item = this.tbl_datail.DataArray[i];
+                    if((item.referencia??"").trim()=="")
+                    {
+                        alert(`Debe colocar una referencia, fila ${i+1}`);
+                        return false;
+                    }
+                    if(!item.categoria || (item.categoria??"").trim()=="")
+                    {
+                        alert(`Debe seleccionar una categoría, fila ${i+1}`);
+                        return false;
+                    }
+                    if(!item.fpago || (item.fpago??"").trim()=="")
+                    {
+                        alert(`Debe seleccionar una forma de pago, fila ${i+1}`);
+                        return false;
+                    }
+                    
+                    if(item.pago==1)
+                    {
+                        if(!item.documento || (item.documento??"").trim()=="")
+                        {
+                            alert(`Debe seleccionar un documento, fila ${i+1}`);
+                            return false;
+                        }
+                        if(!item.cuenta || (item.cuenta??"").trim()=="")
+                        {
+                            alert(`Debe seleccionar una cuenta, fila ${i+1}`);
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        if(!item.tipo_documento || (item.tipo_documento??"").trim()=="")
+                        {
+                            alert(`Debe seleccionar un tipo de documento, fila ${i+1}`);
+                            return false;
+                        }
+                    }
+                }
+                break;
+            default:
+                for (let i = 0; i < this.tbl_datail.DataArray.length; i++) 
+                {
+                    var item = this.tbl_datail.DataArray[i];
+                    if((item.codigo??"").trim()=="")
+                    {
+                        alert(`Debe colocar un código del producto, fila ${i+1}`);
+                        return false;
+                    }
+                    if((item.descripcion??"").trim()=="")
+                    {
+                        alert(`Debe colocar una descripción del producto, fila ${i+1}`);
+                        return false;
+                    }
+                    if(typeof item.linea === "undefined" || Number(item.linea)<1)
+                    {
+                        alert(`Debe seleccionar una línea, fila ${i+1}`);
+                        return false;
+                    }
+                    if(typeof item.divisa === "undefined" || Number(item.divisa)<1)
+                    {
+                        alert(`Debe seleccionar una divisa, fila ${i+1}`);
+                        return false;
+                    }
+                    if(typeof item.impuesto === "undefined" || Number(item.impuesto)<1)
+                    {
+                        alert(`Debe seleccionar un impuesto, fila ${i+1}`);
+                        return false;
+                    }
+                    if(typeof item.clase === "undefined" || Number(item.clase)<1)
+                    {
+                        alert(`Debe seleccionar una clase, fila ${i+1}`);
+                        return false;
+                    }
+                    if(typeof item.tipo === "undefined" || Number(item.tipo)<1)
+                    {
+                        alert(`Debe seleccionar un tipo, fila ${i+1}`);
+                        return false;
+                    }
+                    if(typeof item.metodovaluacion === "undefined" || Number(item.metodovaluacion)<1)
+                    {
+                        alert(`Debe seleccionar un método de valuación, fila ${i+1}`);
+                        return false;
+                    }
+                }
+                break;
+        }
+        return true;
     },
     Procesar()
     {
@@ -31,50 +142,7 @@ var load_cfdi=
             return;
         }
 
-        for (let i = 0; i < this.tbl_datail.DataArray.length; i++) 
-        {
-            var item = this.tbl_datail.DataArray[i];
-            if((item.codigo??"").trim()=="")
-            {
-                alert(`Debe colocar un código del producto, fila ${i+1}`);
-                return;
-            }
-            if((item.descripcion??"").trim()=="")
-            {
-                alert(`Debe colocar una descripción del producto, fila ${i+1}`);
-                return;
-            }
-            if(typeof item.linea === "undefined" || Number(item.linea)<1)
-            {
-                alert(`Debe seleccionar una línea, fila ${i+1}`);
-                return;
-            }
-            if(typeof item.divisa === "undefined" || Number(item.divisa)<1)
-            {
-                alert(`Debe seleccionar una divisa, fila ${i+1}`);
-                return;
-            }
-            if(typeof item.impuesto === "undefined" || Number(item.impuesto)<1)
-            {
-                alert(`Debe seleccionar un impuesto, fila ${i+1}`);
-                return;
-            }
-            if(typeof item.clase === "undefined" || Number(item.clase)<1)
-            {
-                alert(`Debe seleccionar una clase, fila ${i+1}`);
-                return;
-            }
-            if(typeof item.tipo === "undefined" || Number(item.tipo)<1)
-            {
-                alert(`Debe seleccionar un tipo, fila ${i+1}`);
-                return;
-            }
-            if(typeof item.metodovaluacion === "undefined" || Number(item.metodovaluacion)<1)
-            {
-                alert(`Debe seleccionar un método de valuación, fila ${i+1}`);
-                return;
-            }
-        }
+        if(!this.ValidateModule())return;
 
         var details=
         {
